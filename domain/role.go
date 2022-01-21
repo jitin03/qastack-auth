e0754171aa5060215039edeada310ea4ed9fc5ef
jitindoriya <jitin.doriya@peak.ai>
2022-01-21 22:35:28 +0530

    update role for event status api

--- domain/role.go
+++ domain/role.go
@@ -20,7 +20,7 @@ func (p RolePermissions) IsAuthorizedFor(role string, routeName string) bool {
 
 func GetRolePermissions() RolePermissions {
 	return RolePermissions{map[string][]string{
-		"admin": {"RunWorkflow", "AddTestRuns", "AddWorkflow", "AllWorkflows", "DeleteRelease", "AllTestCases", "GetAProject", "GetAllProjects", "NewProject", "UpdateProject", "DeleteProject", "NewRelease", "GetAllRelease", "GetRelease", "UpdateRelease", "AddComponent", "AllComponent", "DeleteComponent", "UpdateComponent", "AddTestCase"},
-		"user":  {"GetAProject", "RunWorkflow", "AddWorkflow", "AllWorkflows", "DeleteRelease", "AddTestRuns", "AllTestCases", "GetAllProjects", "NewProject", "UpdateProject", "DeleteProject", "NewRelease", "GetAllRelease", "GetRelease", "UpdateRelease", "AddComponent", "AllComponent", "DeleteComponent", "UpdateComponent", "AddTestCase"},
+		"admin": {"SubscribeToEvent", "RunWorkflow", "AddTestRuns", "AddWorkflow", "AllWorkflows", "DeleteRelease", "AllTestCases", "GetAProject", "GetAllProjects", "NewProject", "UpdateProject", "DeleteProject", "NewRelease", "GetAllRelease", "GetRelease", "UpdateRelease", "AddComponent", "AllComponent", "DeleteComponent", "UpdateComponent", "AddTestCase"},
+		"user":  {"GetAProject", "SubscribeToEvent", "RunWorkflow", "AddWorkflow", "AllWorkflows", "DeleteRelease", "AddTestRuns", "AllTestCases", "GetAllProjects", "NewProject", "UpdateProject", "DeleteProject", "NewRelease", "GetAllRelease", "GetRelease", "UpdateRelease", "AddComponent", "AllComponent", "DeleteComponent", "UpdateComponent", "AddTestCase"},
 	}}
 }
